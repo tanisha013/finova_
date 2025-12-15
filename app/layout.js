4bcbce5f7a0479b395ec7ad55b6a0df51edbb5c8
Tanisha Mukherjee <tanishamukherjee.013@gmail.com>
2025-12-15 22:21:35 +0530

    page layout

---
+++ app/layout.js
@@ -0,0 +1,24 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "finova",
+  description: "One stop Finance Platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className}`}>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
