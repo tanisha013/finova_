ab61248ca31b8ad8bf5da0c932a319ccb058ecd7
Tanisha Mukherjee <tanishamukherjee.013@gmail.com>
2025-12-18 21:36:21 +0530

    inngest setup

---
+++ app/api/inngest/route.js
@@ -0,0 +1,15 @@
+
+import { inngest } from "@/lib/inngest/client";
+import { checkBudgetAlerts, generateMonthlyReports, processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/function";
+import { serve } from "inngest/next";
+
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    processRecurringTransaction,
+    triggerRecurringTransactions, 
+    generateMonthlyReports,
+    checkBudgetAlerts,
+  ]
+});
