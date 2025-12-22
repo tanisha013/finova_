07056cc47f0e2e29698ec34d48846a6ec2df988a
Tanisha Mukherjee <tanishamukherjee.013@gmail.com>
2025-12-22 18:54:50 +0530

    components

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Image from 'next/image';
-import { motion } from 'motion/react';
+import { motion } from 'framer-motion';
 import { Twitter, Linkedin, Github, Heart } from 'lucide-react';
 
 export function Footer() {
